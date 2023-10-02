2e2eac1f2a60e9ac1e0e842b0965d2e6367b483f
ash lea <example@thisismyactual.email>
2023-10-02 17:39:26 -0400

    font stuff

--- src/halfwidth.rs
+++ src/halfwidth.rs
@@ -1,81 +1,68 @@
-use gba::prelude::*;
 use core::fmt::Write;
+use gba::prelude::*;
 
-const FONT_WIDTH: usize = 4;
-const FONT_HEIGHT: usize = 6;
 const TAB_CHARS: usize = 4;
 
-const MAP_WIDTH: usize = 32;
-const MAP_HEIGHT: usize = 32;
-const WRAP_COL: usize = 60;
+const ROWS: usize = 26;
+const COLUMNS: usize = 60;
 
 // 96 printable ascii chars, each using half of a 8x8 4bpp tile
-#[link_section = ".ewram"]
-static LIFONT: [u8; 96*16] = *include_bytes!("lifont-3x5.4bpp");
-
-type DoubleHalfTile = [u16; 2*8];
-
-fn font() -> &'static [DoubleHalfTile] {
-    unsafe { core::slice::from_raw_parts(LIFONT.as_ptr() as *const DoubleHalfTile, 96/2) }
-}
-
-fn charblock_write(tile_index: usize, tile_row_index: usize, value: u16) {
-    unsafe {
-        let tile_ptr = (CHARBLOCK0_4BPP.as_ptr() as *mut DoubleHalfTile)
-            .add(tile_index);
-        (*tile_ptr).as_mut_ptr().add(tile_row_index).write_volatile(value);
-    }
-}
+const LIFONT: &'static [u8] = &include_aligned_bytes!("lifont-3x5-as-8x8.img.lz77").0;
 
 // TODO: support for drawing to a region of the charblock/screenblock
 // (i.e. not necessarily taking over the whole display)
 pub struct TextPainter {
-    pixel_row: usize,
-    pixel_col: usize,
+    row: usize,
+    col: usize,
 }
 
-impl core::fmt::Write for TextPainter {
+impl Write for TextPainter {
     fn write_str(&mut self, text: &str) -> core::fmt::Result {
-        for c in text.chars()
-            .map(|c| c.as_ascii().map(|a| a.to_u8()).unwrap_or(0x7f) as usize)
+        for c in text
+            .chars()
+            .map(|c| c.as_ascii().map(|a| a.to_u8()).unwrap_or(0x7f) as u16)
         {
-            if self.pixel_col >= FONT_WIDTH * WRAP_COL {
-                self.pixel_row += FONT_HEIGHT;
-                self.pixel_col = 0;
+            if self.col >= COLUMNS {
+                self.row += 1;
+                self.col = 0;
             }
-            if (self.pixel_row >> 3) > MAP_HEIGHT {
-                return Err(core::fmt::Error::default())
+
+            if self.row > ROWS {
+                return Err(core::fmt::Error::default());
             }
+
             if c < 0x20 {
                 match c as u8 {
                     // '\b', backspace
                     8 => {
-                        self.pixel_col = self.pixel_col.saturating_sub(FONT_WIDTH);
+                        self.col = self.col.saturating_sub(1);
                     }
                     b'\t' => {
-                        self.pixel_col = (self.pixel_col + 1).next_multiple_of(TAB_CHARS * FONT_WIDTH);
+                        self.col = (self.col + 1).next_multiple_of(TAB_CHARS);
                     }
                     b'\n' => {
-                        self.pixel_row += FONT_HEIGHT;
-                        self.pixel_col = 0; // assume cooked
+                        self.row += 1;
+                        self.col = 0;
                     }
                     b'\r' => {
-                        self.pixel_col = 0;
+                        self.col = 0;
                     }
                     _ => {} // TODO?
                 }
             } else {
-                let font_tile = &font()[(c - 0x20) >> 1];
-                let out_tile_col = self.pixel_col >> 3;
-                let mut out_pixel_row = self.pixel_row;
-                for font_row in font_tile.iter().skip(c & 1).step_by(2).take(FONT_HEIGHT).copied() {
-                    let out_tile_row = out_pixel_row >> 3;
-                    let out_tile_pixel_row = ((out_pixel_row & 7) << 1) + ((self.pixel_col & 4) >> 2);
-                    let idx = out_tile_row * MAP_WIDTH + out_tile_col;
-                    charblock_write(idx, out_tile_pixel_row, font_row);
-                    out_pixel_row += 1;
-                }
-                self.pixel_col += FONT_WIDTH;
+                let fg = TEXT_SCREENBLOCKS.get_frame(16 + (self.col & 1)).unwrap();
+                let bg = TEXT_SCREENBLOCKS.get_frame(18 + (self.col & 1)).unwrap();
+                fg.get_row(self.row)
+                    .unwrap()
+                    .get(self.col >> 1)
+                    .unwrap()
+                    .write(TextEntry::new().with_tile(c - 0x20));
+                bg.get_row(self.row)
+                    .unwrap()
+                    .get(self.col >> 1)
+                    .unwrap()
+                    .write(TextEntry::new().with_tile(1));
+                self.col += 1;
             }
         }
         Ok(())
@@ -84,44 +71,79 @@ impl core::fmt::Write for TextPainter {
 
 impl TextPainter {
     pub const fn new() -> Self {
-        Self {
-            pixel_row: 0,
-            pixel_col: 0,
-        }
+        Self { row: 0, col: 0 }
     }
 
     pub fn setup_display(&mut self) {
-        self.pixel_row = 0;
-        self.pixel_col = 0;
-        let dispcnt = DisplayControl::new()
-            .with_show_bg0(true);
-        DISPCNT.write(dispcnt);
-        let bg0cnt = BackgroundControl::new()
-            .with_size(0)
-            .with_charblock(0)
-            .with_screenblock(16);
-        BG0CNT.write(bg0cnt);
+        self.row = 0;
+        self.col = 0;
+
+        DISPCNT.write(
+            DisplayControl::new()
+                .with_show_bg0(true)
+                .with_show_bg1(true)
+                .with_show_bg2(true)
+                .with_show_bg3(true),
+        );
+
+        BG0CNT.write(
+            BackgroundControl::new()
+                .with_size(0)
+                .with_charblock(0)
+                .with_screenblock(16),
+        );
         BG0HOFS.write(0);
         BG0VOFS.write(0);
-        let screenblock = TEXT_SCREENBLOCKS.get_frame(16).unwrap();
-        let mut x = 0;
-        for r in 0..32 {
-            let row = screenblock.get_row(r).unwrap();
-            for cell in row.iter() {
-                cell.write(TextEntry::new().with_tile(x));
-                x += 1;
+
+        BG1CNT.write(
+            BackgroundControl::new()
+                .with_size(0)
+                .with_charblock(0)
+                .with_screenblock(17),
+        );
+        BG1HOFS.write(252);
+        BG1VOFS.write(0);
+
+        BG2CNT.write(
+            BackgroundControl::new()
+                .with_size(0)
+                .with_charblock(1)
+                .with_screenblock(18),
+        );
+        BG2HOFS.write(0);
+        BG2VOFS.write(0);
+
+        BG3CNT.write(
+            BackgroundControl::new()
+                .with_size(0)
+                .with_charblock(1)
+                .with_screenblock(19),
+        );
+        BG3HOFS.write(252);
+        BG3VOFS.write(0);
+
+        unsafe {
+            LZ77UnCompReadNormalWrite16bit(LIFONT.as_ptr(), CHARBLOCK0_4BPP.as_ptr() as *mut u16);
+        }
+
+        // one tile background
+        CHARBLOCK1_4BPP.get(0).unwrap().write([0x0000ffff; 8]);
+        for screenblock in 16..=19 {
+            let frame = TEXT_SCREENBLOCKS.get_frame(screenblock).unwrap();
+            for r in 0..32 {
+                let row = frame.get_row(r).unwrap();
+                for cell in row.iter() {
+                    cell.write(TextEntry::new());
+                }
             }
         }
-        for tile in CHARBLOCK0_4BPP.iter() {
-            tile.write([0; 8]);
-        }
-        BG_PALETTE.index(1).write(Color::new().with_red(22).with_green(16).with_blue(16));
-        BG_PALETTE.index(2).write(Color::new().with_red(31).with_green(31).with_blue(31));
+
+        BG_PALETTE
+            .index(1)
+            .write(Color::new().with_red(22).with_green(16).with_blue(16));
+        BG_PALETTE
+            .index(2)
+            .write(Color::new().with_red(31).with_green(31).with_blue(31));
         BG_PALETTE.index(0).write(Color::new().with_red(8));
     }
-
-    pub fn paint_text(&mut self, text: &str) {
-        let _ = (*self).write_str(text).ok();
-    }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@
 
 use ape_fatfs::fs::{FileSystem, FsOptions};
 use ape_mbr::{PartitionId, MBR};
-use core::fmt::Write;
+use core::{fmt::Write, str::from_utf8_unchecked};
 use ezflash::set_led_control;
 use fs::BufferedIo;
 use gba::prelude::*;
 use halfwidth::TextPainter;
+use log::{error, info, Log};
 use sd::SdCard;
 
 mod dma;
@@ -21,6 +22,23 @@ mod fs;
 mod halfwidth;
 mod sd;
 
+static mut PAINTER: TextPainter = TextPainter::new();
+static LOGGER: ScreenLogger = ScreenLogger;
+
+struct ScreenLogger;
+
+impl Log for ScreenLogger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        unsafe { writeln!(PAINTER, "{} - {}", record.level(), record.args()).unwrap() };
+    }
+
+    fn flush(&self) {}
+}
+
 #[allow(unused_must_use)]
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
@@ -28,24 +46,15 @@ fn panic_handler(info: &core::panic::PanicInfo) -> ! {
         // red+green
         set_led_control(0b10100000);
     }
-    BACKDROP_COLOR.write(Color::RED);
+    BACKDROP_COLOR.write(Color::new().with_red(8));
 
-    let mut text_painter = TextPainter::new();
-    text_painter.setup_display();
-
-    text_painter.write_str("panic at ");
     if let Some(location) = info.location() {
-        write!(&mut text_painter, "{}:{}", location.file(), location.line());
+        error!("panic at {}:{}:", location.file(), location.line());
     } else {
-        text_painter.write_str("unknown location");
+        error!("panic at unknown location:");
     };
     if let Some(msg) = info.message() {
-        write!(&mut text_painter, ":\n");
-        core::fmt::write(&mut text_painter, *msg);
-    }
-    if let Some(s) = info.payload().downcast_ref::<&str>() {
-        text_painter.write_str("\n");
-        text_painter.write_str(s);
+        error!("{}", *msg);
     }
 
     loop {
@@ -53,75 +62,43 @@ fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     }
 }
 
-// static LOGGER: ScreenLogger = ScreenLogger::new();
-
-// struct ScreenLogger {
-//     buf: GbaCell<[u8; 1024]>,
-//     len: GbaCell<u16>,
-//     pos: GbaCell<u16>,
-// }
-
-// impl ScreenLogger {
-//     fn new() -> Self {
-//         Self {
-//             buf: GbaCell::new([0; 1024]),
-//             len: GbaCell::new(0),
-//             pos: GbaCell::new(0),
-//         }
-//     }
-// }
-
-// impl Log for ScreenLogger {
-//     fn enabled(&self, metadata: &log::Metadata) -> bool {
-//         true
-//     }
-
-//     fn log(&self, record: &log::Record) {}
-
-//     fn flush(&self) {
-//         todo!()
-//     }
-// }
-
-extern "C" fn irq_handler(_: IrqBits) {}
-
-fn _asdf(text: &str) {
-    Cga8x8Thick.bitunpack_4bpp(CHARBLOCK0_4BPP.as_region(), 0);
-    BG0CNT.write(BackgroundControl::new().with_size(2).with_screenblock(8));
-
-    let screenblock = TEXT_SCREENBLOCKS.get_frame(8).unwrap();
-    for x in 0..32 {
-        for y in 0..32 {
-            screenblock
-                .get(x, y)
-                .unwrap()
-                .write(TextEntry::new().with_tile(0));
+extern "C" fn irq_handler(irq: IrqBits) {
+    if irq.hblank() {
+        let next_vcount = VCOUNT.read() + 1;
+        // overscan region
+        if next_vcount > 160 {
+            BG0VOFS.write(0);
+            BG1VOFS.write(0);
+            BG2VOFS.write(0);
+            BG3VOFS.write(0);
+        } else if next_vcount % 6 == 0 {
+            BG0VOFS.write(next_vcount / 3);
+            BG1VOFS.write(next_vcount / 3);
+            BG2VOFS.write(next_vcount / 3);
+            BG3VOFS.write(next_vcount / 3);
         }
     }
-
-    for (y, line) in text.split_terminator("\n").take(32).enumerate() {
-        let row = screenblock.get_row(y).unwrap();
-        for (x, byte) in line.bytes().take(32).enumerate() {
-            let text_entry = TextEntry::new().with_tile(byte as u16);
-            row.get(x).unwrap().write(text_entry);
-        }
-    }
-
-    DISPCNT.write(DisplayControl::new().with_show_bg0(true));
 }
 
 #[no_mangle]
 extern "C" fn main() -> ! {
     RUST_IRQ_HANDLER.write(Some(irq_handler));
-    DISPSTAT.write(DisplayStatus::new().with_irq_vblank(true));
-    IE.write(IrqBits::VBLANK);
+    DISPSTAT.write(
+        DisplayStatus::new()
+            .with_irq_vblank(true)
+            .with_irq_hblank(true),
+    );
+    IE.write(IrqBits::new().with_vblank(true).with_hblank(true));
     IME.write(true);
 
     DISPCNT.write(DisplayControl::new().with_show_bg0(true));
-    BACKDROP_COLOR.write(Color::YELLOW);
 
-    let mut text_painter = TextPainter::new();
-    text_painter.setup_display();
+    unsafe {
+        PAINTER.setup_display();
+        BACKDROP_COLOR.write(Color::new().with_green(8));
+        log::set_logger_racy(&LOGGER).unwrap();
+        log::set_max_level_racy(log::LevelFilter::Trace);
+    }
 
     //panic!("According to all known laws of aviation, there is no way for a bee to fly.\n\tHowever\rThe bee flies anyway because fuck you that's why.");
 
@@ -130,13 +107,12 @@ extern "C" fn main() -> ! {
         set_led_control(0b10110001);
     }
 
-    text_painter.paint_text("hello world!");
+    info!("hello world!");
 
     let mut mbr = MBR::new(BufferedIo::<512, 2048, _>::new(SdCard)).unwrap();
     let partition = mbr.get_partition(PartitionId::One).unwrap();
     let fs = FileSystem::new(partition, FsOptions::new()).unwrap();
 
-    BACKDROP_COLOR.write(Color::GREEN);
     unsafe {
         // green + blue sd indicator
         set_led_control(0b10010001);
@@ -162,7 +138,7 @@ extern "C" fn main() -> ! {
             pos += 1;
         }
 
-        text_painter.paint_text(unsafe { from_utf8_unchecked(&bytes) });
+        info!("{}", unsafe { from_utf8_unchecked(&bytes) });
     }
 
     loop {}
